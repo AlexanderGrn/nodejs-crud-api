bfef11893e9bd65ef9e116ec525a109a46c91745
AlexanderGrn <s.green@list.ru>
2022-06-19 21:03:08 +0300

    feat: add handling error 500

--- server.js
+++ server.js
@@ -33,31 +33,35 @@ import {
 //     }
 // ];
 
-const errors = {
-    '404': 'Rout not found'
-};
+// const errors = {
+//     '404': 'Rout not found',
+//     '500': 'Server side error'
+// };
 
 const server = http.createServer((request, response) => {
+    try {
+        if (request.url === `/api/users` && request.method === 'GET') {
+            getUsers(request, response);
 
-    if (request.url === `/api/users` && request.method === 'GET') {
-        getUsers(request, response);
+        } else if (request.url.match(/\/api\/users\/([0-9]+)/) && request.method === 'GET') {
+            const userId = request.url.split('/')[3];
+            getUser(request, response, userId);
 
-    } else if (request.url.match(/\/api\/users\/([0-9]+)/) && request.method === 'GET') {
-        const userId = request.url.split('/')[3];
-        getUser(request, response, userId);
+        } else if (request.url === '/api/users' && request.method === 'POST') {
+            reateUser(request, response);
 
-    } else if (request.url === '/api/users' && request.method === 'POST') {
-        createUser(request, response);
+        } else if (request.url.match(/\/api\/users\/([0-9]+)/) && request.method === 'PUT') {
+            const userId = request.url.split('/')[3];
+            updateUser(request, response, userId);
 
-    } else if (request.url.match(/\/api\/users\/([0-9]+)/) && request.method === 'PUT') {
-        const userId = request.url.split('/')[3];
-        updateUser(request, response, userId);
-
-    } else {
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.end(errors['404']);
+        } else {
+            response.writeHead(404, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify({ message: "Rout not found" }));
+        }
+    } catch (err) {
+        response.writeHead(500, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ message: "Server side error" }));
     }
-
 });
 
 const PORT = process.env.PORT || 5000;
